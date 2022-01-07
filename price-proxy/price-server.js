b7b4274c04f7466158cb89f400426ab7871d5323
reemuru <rimuru@hiahatf.org>
2022-01-07 18:43:51 -0500

    price-proxy: add helmet and disable x-powered-by

--- price-proxy/price-server.js
+++ price-proxy/price-server.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const helmet = require('helmet')
 const axios = require('axios')
 const APP = express()
 const PORT = process.env.PORT || 7777
 // set API_KEY env variable first
 const API_KEY = process.env.PRICE_PROXY_API_KEY
 const URL = `https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC&api_key=${API_KEY}`
+
+APP.disable('x-powered-by');
+// add helmet for hardening
+APP.use(helmet({
+  frameguard: {
+    action: 'deny'
+  },
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+    }
+  },
+  dnsPrefetchControl: false
+}));
 
 APP.get('/price/xmr', (req, res) => {
     axios.get(URL)
