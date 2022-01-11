e68492d53b6bfdd6cd16ba4ce655c4b0b3e77326
reemuru <rimuru@hiahatf.org>
2022-01-11 17:26:54 -0500

    anonima: test json response

--- anonima/main.cpp
+++ anonima/main.cpp
@@ -1,16 +1,51 @@
 #include "crow.h"
 #include "json.hpp"
+#include <cstdio>
+#include <string>
 
 using json = nlohmann::json;
+using namespace std;
+
+namespace anonima
+{
+struct htmlresponse: public crow::response
+{
+    htmlresponse(string&& _body)
+            : crow::response {std::move(_body)}
+    {
+        add_header("Content-Type", "text/html; charset=utf-8");
+    }
+};
+
+struct jsonresponse: public crow::response
+{
+    jsonresponse(const nlohmann::json& _body)
+            : crow::response {_body.dump()}
+    {
+        add_header("Access-Control-Allow-Origin", "*");
+        add_header("Access-Control-Allow-Headers", "Content-Type");
+        add_header("Content-Type", "application/json");
+    }
+};
+}
 
 int main()
 {
     crow::SimpleApp app;
 
+
     CROW_ROUTE(app, "/")([](){
         // sample json use
-        json j = "{\"test\":\"value\"}";
-        return j;
+        struct test {
+            string value;
+        };
+        struct test a;
+        a.value = "test";
+        test *b = &a;
+        json j;
+        j["key"] = b->value;
+        anonima::jsonresponse r{j};
+        return r;
     });
 
     app.port(4321).run();
