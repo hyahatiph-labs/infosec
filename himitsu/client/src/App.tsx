5d457cf12e89c28b00e313852a9d1fa66388ceb8
reemuru <rimuru@hiahatf.org>
2022-02-21 18:36:37 -0500

    himitsu: refactor and fix build

--- himitsu/client/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from 'react';
-import MainComponent from './Components/Main/MainComponent';
-
-const App: React.FC = (): ReactElement => <><MainComponent /></>;
-
-export default App;
