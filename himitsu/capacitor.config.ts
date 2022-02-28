bc49507fdd49967607aa2832133c75cfbcd2cbd7
reemuru <rimuru@hiahatf.org>
2022-02-28 06:24:47 -0500

    himitsu: add capacitor for android builds

---
+++ himitsu/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.hiahatf.himitsu',
+  appName: 'himitsu',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
