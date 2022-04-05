8ff31704f5b3aa6cde775d347b17783c35a2e7ac
reemuru <rimuru@hiahatf.org>
2022-04-05 12:03:14 -0500

    analitiko: cleanup models and add tables

--- analitiko/scripts/xmr-ring-output-network-graph/app.R
+++ analitiko/scripts/xmr-ring-output-network-graph/app.R
@@ -11,7 +11,7 @@ library(DBI)
 library(RODBCDBI)
 library(tibble)
 library(readr)
-# create an /infosec/analitiko/.Renviron file with 
+# create an /infosec/analitiko/.Renviron file with
 # PG_USER=<postgresql username>
 # PG_CRED=<postgresql password>
 # PG_DB_NAME=<database_name>
@@ -23,22 +23,22 @@ pg_cred <- Sys.getenv("PG_CRED")
 shiny_port <- Sys.getenv("SHINY_PORT")
 pg_db_name <- Sys.getenv("PG_DB_NAME")
 con <- dbConnect(odbc::odbc(), driver = "PostgreSQL",Server = "127.0.0.1",
-                 Database = pg_db_name, UID = pg_user, PWD = pg_cred, Port = 5432)
+                 Database = pg_db_name, UID = pg_user, PWD = pg_cred,
+                 Port = 5432)
 
 # Data Viz Libraries
 library(igraph)
 library(tidygraph)
 library(networkD3)
 
-block_time = 120000
+block_time <- 120000
 # This function is used to wrap the extract, transact load process
 # for automation inside of the Shiny app server
 etl <- function() {
-  # create a data frame giving the hierarchical structure of your individuals. 
+  # create a data frame giving the hierarchical structure of your individuals.
   # Origin on top, then groups, then subgroups
-  
   # D1 - from (block height) | to (tx hash)
-  qD1 <- dbSendQuery(con, 
+  qD1 <- dbSendQuery(con,
                      'SELECT
                     t1.height,
                     t2.hash
@@ -52,43 +52,38 @@ etl <- function() {
                     t1."ringOutputIndices"
                     FROM "Txes" t1')
   rD2 <- dbFetch(qD2)
-  
   # close but let us create a new data frame with individual
   # ring output index per hash
   hash <- NULL
   index <- NULL
-  t = 1
-  j = 1
+  t <- 1
+  j <- 1
   for (h in rD2$hash) {
-    v1 <- strsplit(rD2$ringOutputIndices[t], split="[{}]")
+    v1 <- strsplit(rD2$ringOutputIndices[t], split = "[{}]")
     v2 <- unlist(v1[1])[2]
-    v3 <- unlist(strsplit(v2, split=","))
+    v3 <- unlist(strsplit(v2, split = ","))
     for (r in v3) {
       hash[j] <- h
       index[j] <- readr::parse_number(r)
-      j = j + 1
+      j <- j + 1
     }
-    t = t + 1
+    t <- t + 1
   }
-  
-  v_hash = unlist(hash)
-  v_index = unlist(index)
+
+  v_hash <- unlist(hash)
+  v_index <- unlist(index)
   d1 <- NULL
   d2 <- NULL
   d1 <- data.frame(from = rD1$height, to = rD1$hash)
   d2 <- data.frame(from = v_hash, to = v_index)
-  
   # Hierarchical Network Graph
   hierarchy <- rbind(d1, d2)
   hierarchy <- na.omit(hierarchy)
-  
   # TODO: benchmarking with samples. Need a machine that can render this
   set.seed(6969)
   hierarchy[sample(nrow(hierarchy), 10000, replace = FALSE, prob = NULL),]
 }
-
 hierarchy <- etl()
-
 length(hierarchy$from)
 
 # This is a Shiny web application. You can run the application by clicking
@@ -100,7 +95,6 @@ length(hierarchy$from)
 #
 
 library(shiny)
-library(networkD3)
 port <- readr::parse_integer(shiny_port)
 options(shiny.port = port)
 # Define UI for application that draws a histogram
@@ -109,7 +103,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("Monero Ring Output Indices Network Graph"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("txs",
@@ -134,7 +128,8 @@ ui <- fluidPage(
 
     output$nPlot <- renderSimpleNetwork({
         # draw the plot with the specified number of transactions
-        simpleNetwork(hierarchy[1:input$txs,], width = "100px", height = "100px",
+        simpleNetwork(hierarchy[1:input$txs,], width = "100px",
+                          height = "100px",
                            Source = 1,                 # column number of source
                            Target = 2,                 # column number of target
                            linkDistance = 10,          # distance between node.
@@ -144,7 +139,7 @@ ui <- fluidPage(
                            linkColour = "#000",        # color of edges
                            nodeColour = "#FF5722",     # color of nodes
                            opacity = 0.9,              # opacity of nodes.
-                           zoom = T                    # Can you zoom on the figure?
+                           zoom = T                    # Enable zoom
         )
     })
 
