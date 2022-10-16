6cca1dca2dd746ab41d7e68efed5912bfb09384c
rimuru <rimuru@hiahatf.org>
2022-10-16 14:06:42 -0400

    scripts: update package installation in examples

---
+++ analitiko/scripts/xmr-ring-output-network-graph/packages.R
@@ -0,0 +1,5 @@
+# install packages as required
+my_packages <- c("DBI", "RODBCDBI", "odbc", "readr", "igraph", "tidygraph", "networkD3", "shiny")
+not_installed <- my_packages[!(my_packages %in% installed.packages()[ , "Package"])]
+if(length(not_installed))
+  install.packages(not_installed, repos='http://cran.us.r-project.org')
